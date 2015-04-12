fa6d39602014370c89fbe1d95fdf53ad6cf69e0b
SACRELEE <sacrelee@gmail.com>
2015-04-12 17:03:06 +0800

    Initial Commit

---
+++ PageViewControllerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PageViewControllerDemo
+//
+//  Created by SACRELEE on 15/4/12.
+//  Copyright (c) 2015年 Sumtice：http://sacrelee.me. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
